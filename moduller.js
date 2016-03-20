f8ccee5991e49141cb2277b3608856633b0c5690
Volodymyr Gamula <v.gamula@gmail.com>
2016-03-20 16:55:49 +0200

    Revert "Added ability to emit events by using on/trigger functions"
    This reverts commit 90a0ddd4725eba444b059584b661c5693c0e71e3.

--- moduller.js
+++ moduller.js
@@ -1,45 +1,21 @@
-(function(define) {
-    return define(function() {
-        var events = '__events';
-        return function() {
-            return {
-                module: function(path, cb) {
-                    var dest = this;
-                    var chunks = path.split('.');
-                    if (chunks[0] === 'module') {
-                        console.error('You have no ability to set path like `module`!');
-                        return false;
-                    }
-                    chunks.forEach(function(chunk) {
-                        if (!dest[chunk]) {
-                            dest[chunk] = {};
-                            dest[chunk][events] = {};
-                            const self = dest[chunk];
-                            self['on'] = function(name, func) {
-                                self[events][name] = func;
-                            };
-                            self['trigger'] = function() {
-                                if (arguments.length) {
-                                    funcName = arguments[0];
-                                    if (typeof self[events][funcName] === 'function') {
-                                        self[events][funcName].apply(self, Array.prototype.slice.call(arguments, 1));
-                                    }
-                                };
-                            };
-                        }
-                        dest = dest[chunk];
-                    });
-                    if (cb && typeof cb === 'function') {
-                        cb(dest);
-                    }
+window.Moduller = function () {
+    return {
+        module: function(path, cb) {
+            var dest = this;
+            var chunks = path.split('.');
+            if (chunks[0] === 'module') {
+                console.error('You have no ability to set path like `module`!');
+                return false;
+            }
+            chunks.forEach(function(chunk) {
+                if (!dest[chunk]) {
+                    dest[chunk] = {};
                 }
-            };
+                dest = dest[chunk];
+            });
+            if (cb && typeof cb === 'function') {
+                cb(dest);
+            }
         }
-    });
-})((typeof define !== 'undefined' ? define : function(factory) {
-    if (typeof module !== 'undefined' && typeof exports !== 'undefined') {
-        return module.exports = factory();
-    } else {
-        return window.Moduller = factory();
-    }
-}));
+    };
+};
