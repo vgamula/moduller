9d87ebae4d001776b731e6f05dbe81c803c24e8a
Volodymyr Gamula <v.gamula@gmail.com>
2016-04-15 11:11:16 +0300

    Improved initializing moduller

--- moduller.js
+++ moduller.js
@@ -1,21 +1,32 @@
-window.Moduller = function () {
-    return {
-        module: function(path, cb) {
-            var dest = this;
-            var chunks = path.split('.');
-            if (chunks[0] === 'module') {
-                console.error('You have no ability to set path like `module`!');
-                return false;
-            }
-            chunks.forEach(function(chunk) {
-                if (!dest[chunk]) {
-                    dest[chunk] = {};
+(function(define) {
+    return define(function() {
+        return function() {
+            return {
+                module: function(path, cb) {
+                    var dest = this;
+                    var chunks = path.split('.');
+                    if (chunks[0] === 'module') {
+                        console.error('You have no ability to set path like `module`!');
+                        return false;
+                    }
+                    chunks.forEach(function(chunk) {
+                        if (!dest[chunk]) {
+                            dest[chunk] = {};
+                        }
+                        dest = dest[chunk];
+                    });
+                    if (cb && typeof cb === 'function') {
+                        cb(dest);
+                    }
                 }
-                dest = dest[chunk];
-            });
-            if (cb && typeof cb === 'function') {
-                cb(dest);
-            }
-        }
-    };
-};
+            };
+        };
+    });
+})(typeof define !== 'undefined' ? define : function(factory) {
+    debugger;
+    if (typeof module !== 'undefined' && typeof exports !== 'undefined') {
+        return module.exports = factory();
+    } else {
+        return window.Moduller = factory();
+    }
+});
